0cd4f1bcfff5974142672c74bcea7edcd9ae667b
Rolf Turner <r.turner@auckland.ac.nz>
2009-09-04 00:00:00 +0000

    version 0.0-7

--- R/First.R
+++ R/First.R
@@ -1,8 +1,6 @@
-.First.lib <- function(lib,pkg) {
+.onLoad <- function(lib, pkg) {
 	library.dynam("Iso", pkg, lib)
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
         cat(paste(pkg, ver, "\n"))
-        cat("Note: A new argument \"stepfun\" has been added to\n")
-        cat("pava() and pava.sa().  A new argument \"type\" has been\n")
-	cat("added to ufit().  See the documentation.\n")
+        cat("Note: This package now has a NAMSPACE.\n")
 }
