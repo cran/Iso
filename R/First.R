c28c670f2c74a011f486f96b19915ae8bf7113a2
Rolf Turner <r.turner@auckland.ac.nz>
2011-11-04 00:00:00 +0000

    version 0.0-10

--- R/First.R
+++ R/First.R
@@ -1,6 +1,5 @@
 .onLoad <- function(lib, pkg) {
 	library.dynam("Iso", pkg, lib)
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
-        cat(paste(pkg, ver, "\n"))
-        cat("Note: This package now has a NAMESPACE.\n")
+        packageStartupMessage(paste(pkg, ver))
 }
